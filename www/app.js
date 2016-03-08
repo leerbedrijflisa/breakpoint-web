1d6ebb61d4429f3270fabaee6645e50f076fc6be
Patrick <patrickhuiskens@gmail.com>
2016-03-08 09:58:02 +0100

    start of create page

--- www/app.js
+++ www/app.js
@@ -1,7 +1,8 @@
 export class App {
 	configureRouter(config, router) {
 		config.map([
-			{ route: '', moduleId: 'bug-reports/list' }
+			{ route: '', moduleId: 'bug-reports/list' },
+            { route: 'create', moduleId: 'bug-reports/create' }
 		])
 	}
 }
