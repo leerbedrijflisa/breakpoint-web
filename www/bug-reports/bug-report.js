02782def068d0806968789894b2b174b81b86a21
Patrick <patrickhuiskens@gmail.com>
2016-05-10 10:02:58 +0200

    Ticket 5 progress. Can now go to a report page and get info. (console log)

--- www/bug-reports/bug-report.js
+++ www/bug-reports/bug-report.js
@@ -4,7 +4,16 @@ export class bugReport {
     constructor() {
     }
    
-   activate(params) {
+async activate(params) {
+        console.log(params.id);
+        params.id;
 
+        this.id = params.id;
+        var db = new Database();
+        let currentBug = await db.getBugReportInfo(params.id);
+
+        this.currentBug = currentBug;
+
+        console.log(currentBug);
    } 
 }
